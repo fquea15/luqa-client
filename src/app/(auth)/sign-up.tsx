350c9d817088cd9dfc342d745aafeb91db20a82a
DJDCV <deivid.delcarpio@tecsup.edu.pe>
2025-06-10 00:33:23 -0500

    Correcciones

--- src/app/(auth)/sign-up.tsx
+++ src/app/(auth)/sign-up.tsx
@@ -5,7 +5,7 @@ import { registerUser } from "@/shared/services/authService";
 import { useRouter } from 'expo-router';
 import { LockIcon, MailIcon, UserIcon } from 'lucide-react-native';
 import React, { useState } from 'react';
-import { Alert, Image, ScrollView, Text, View } from 'react-native';
+import { Alert, Image, Text, View } from 'react-native';
 
 console.log(process.env.EXPO_PUBLIC_API_URL)
 
@@ -45,72 +45,70 @@ export default function SignUpScreen() {
   }
 
   return (
-    <ScrollView className="flex-1 bg-neutral-50">
-      <View className="mx-[30px] flex-1 justify-center">
-        <View className={"mt-32 h-[100px] flex-row justify-center"}>
-          <Image source={images.logoMain} className={"h-[100px] w-[250px]"} resizeMode={"cover"} />
-        </View>
-        <View className="mt-[40px]">
-          <Text className="text-[32px] font-semibold text-primary-500">Crea tu cuenta</Text>
-        </View>
-
-        <View className="mt-2">
-          <InputField
-            placeholder="Nombre completo"
-            icon={UserIcon}
-            value={name}
-            onChangeText={setName}
-            containerStyle={"rounded-[10px] "}
-            inputStyle={"placeholder:text-textSecondary-400"}
-          />
-          <InputField
-            placeholder="Correo electrónico"
-            icon={MailIcon}
-            value={email}
-            onChangeText={setEmail}
-            containerStyle={"rounded-[10px] "}
-            inputStyle={"placeholder:text-textSecondary-400"}
-          />
-          <InputField
-            placeholder="Contraseña"
-            icon={LockIcon}
-            secureTextEntry
-            value={password}
-            onChangeText={setPassword}
-            containerStyle={"rounded-[10px] "}
-            inputStyle={"placeholder:text-textSecondary-400"}
-          />
-          <InputField
-            placeholder="Repite la contraseña"
-            icon={LockIcon}
-            secureTextEntry
-            value={confirmPassword}
-            onChangeText={setConfirmPassword}
-            medium
-            containerStyle={"rounded-[10px] "}
-            inputStyle={"placeholder:text-textSecondary-400"}
-          />
-        </View>
-
-        <View className="mt-10 flex-1 flex-col gap-6">
-          <CustomButton
-            title={loading ? 'Registrando...' : 'Registrarse'}
-            onPress={handleRegister}
-            disabled={loading}
-            bgVariant="secondary"
-          />
-
-          <Text className="mt-10 text-center text-base text-textSecondary-500">
-            ¿Tienes una cuenta?{' '}
-            <Text
-              onPress={() => router.push('/(auth)/sign-in')}
-              className="text-primary-500 font-semibold"
-            >
-              Inicia Sesión
-            </Text>
-          </Text>
-        </View>
+    <View className="mx-[30px] flex-1 justify-center">
+      <View className={"mt-32 h-[100px] flex-row justify-center"}>
+        <Image source={images.logoMain} className={"h-[100px] w-[250px]"} resizeMode={"cover"} />
       </View>
-    </ScrollView>
+      <View className="mt-[40px]">
+        <Text className="text-[32px] font-semibold text-primary-500">Crea tu cuenta</Text>
+      </View>
+
+      <View className="mt-2">
+        <InputField
+          placeholder="Nombre completo"
+          icon={UserIcon}
+          value={name}
+          onChangeText={setName}
+          containerStyle={"rounded-[10px] "}
+          inputStyle={"placeholder:text-textSecondary-400"}
+        />
+        <InputField
+          placeholder="Correo electrónico"
+          icon={MailIcon}
+          value={email}
+          onChangeText={setEmail}
+          containerStyle={"rounded-[10px] "}
+          inputStyle={"placeholder:text-textSecondary-400"}
+        />
+        <InputField
+          placeholder="Contraseña"
+          icon={LockIcon}
+          secureTextEntry
+          value={password}
+          onChangeText={setPassword}
+          containerStyle={"rounded-[10px] "}
+          inputStyle={"placeholder:text-textSecondary-400"}
+        />
+        <InputField
+          placeholder="Repite la contraseña"
+          icon={LockIcon}
+          secureTextEntry
+          value={confirmPassword}
+          onChangeText={setConfirmPassword}
+          medium
+          containerStyle={"rounded-[10px] "}
+          inputStyle={"placeholder:text-textSecondary-400"}
+        />
+      </View>
+
+      <View className="mt-10 flex-1 flex-col gap-6">
+        <CustomButton
+          title={loading ? 'Registrando...' : 'Registrarse'}
+          onPress={handleRegister}
+          disabled={loading}
+          bgVariant="secondary"
+        />
+
+        <Text className="mt-10 text-center text-base text-textSecondary-500">
+          ¿Tienes una cuenta?{' '}
+          <Text
+            onPress={() => router.push('/(auth)/sign-in')}
+            className="text-primary-500 font-semibold"
+          >
+            Inicia Sesión
+          </Text>
+        </Text>
+      </View>
+    </View>
   );
 }
